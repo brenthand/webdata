c10889e05c07066bd3d7d96bba81b5e2733a97b3
Brent hand <hand.brent@gmail.com>
2016-05-29 18:28:17 -0400

    added main method

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from webdata import Parser
+import urllib.request
 
 
-def main():
+def parse(html):
 
-    with open('test.html', 'r') as myfile:
-        data=myfile.read()
+    """with open('test.html' , 'r') as myfile:
+        html=myfile.read()"""
 
     p = Parser()
-    p.feed(data)
+    p.feed(html)
 
-    print("Word Count: " + str(p.WORD_COUNT))
+    #print("Word Count: " + str(p.WORD_COUNT))
+    return p.WORD_COUNT
+
+def webdata(t, list):
+    wordcount = 0
+    if t == 'f':
+        for item in list:
+            with open(item, 'r') as f:
+                wordcount += parse(f.read())
+    if t == 'u':
+        for url in list:
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+                wordcount += parse(str(html))
 
 
-main()
+    print('Wordcount: ' + str(wordcount))
+
+
+
+
+#l = ['test.html', 'test2.html', 'test3.html']
+l = ['http://novastellatranslations.com']
+webdata('u', l)
